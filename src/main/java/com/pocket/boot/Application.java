c935d5e90c867473f419959d384302795f79972a
jungdahee <jungdahee010@naver.com>
2021-03-27 23:53:03 +0900

    Refactor Application.java

--- src/main/java/com/pocket/boot/Application.java
+++ src/main/java/com/pocket/boot/Application.java
@@ -1,4 +1,12 @@
 package com.pocket.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @SpringBootApplication -> 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동 설정
 public class Application {
+    public static void main(String args[]){
+        SpringApplication.run(Application.class, args);
+    }
 }
